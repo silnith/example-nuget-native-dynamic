654238359f5070adb7effad1533161d156360551
silnith <silnith@gmail.com>
2026-03-24 21:54:22 -0500

    Add unit test project.

---
+++ silnith.tessellation.dynamic.tests/silnith.tessellation.dynamic.tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace silnithtessellationdynamictests
+{
+	TEST_CLASS(silnithtessellationdynamictests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
